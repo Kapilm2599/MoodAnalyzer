8ae6f4ad9f0b647904534f42fc3a0089c38e2352
Kapilm2599 <mahajankapil575@gmail.com>
2023-05-14 18:27:53 +0530

    Added: Handle exception if user provides invalid mood

--- src/com/bridgelabz/MoodAnalyser.java
+++ src/com/bridgelabz/MoodAnalyser.java
@@ -7,29 +7,41 @@ public class MoodAnalyser {
      String message;
 
 	 public MoodAnalyser(String message) {
+		 if(message.equalsIgnoreCase("null")) {
+			 this.message = null;
+		 }
+		 else {
 	        this.message = message;
-	    }
+	        
+	   	  }
+		 
+	 	}
 
 	    public MoodAnalyser() {
 
 	    }
 	    
-	    public String moodAnalysis() {
-	        boolean status = message.toLowerCase().contains("sad");
-	        if (status == true) {
-	            return "Sad";
-	        } else {
-	            return "Happy";
-	        }
-	    }
-
+	    public void moodAnalysis() {
+	    	try {
+	    		
+	    		boolean status = message.toLowerCase().contains("sad");
+	    		if (status == true) {
+	    			System.out.println("Sad");
+	    		} else {
+	    			System.out.println("Happy");
+	    		}
+	    	}
+	    	catch(Exception exception) {
+	    		System.out.println("Happy");
+	    	}
+	    	
+	    }	
 	    public static void main(String[] args) {
 	        Scanner input = new Scanner(System.in);
 	        System.out.print("Enter message : ");
 	        String message = input.nextLine();
 	        MoodAnalyser moodAnalyser = new MoodAnalyser(message);
-	        String result = moodAnalyser.moodAnalysis();
-	        System.out.println(result);
+	        moodAnalyser.moodAnalysis();
 	    }
 
 }
